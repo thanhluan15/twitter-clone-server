86af1bb1e4d517ab361c25fb15f376727004c3f6
Siesta1510 <93849920+Siesta1510@users.noreply.github.com>
2023-06-02 16:22:40 +0700

    feat(api): update api

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, HTTPException
 from datetime import date
 from typing import Union
+from datetime import datetime
+import uuid
 
 from fastapi.middleware.cors import CORSMiddleware
 from libs.supabase import create_supabase_client
@@ -15,6 +17,9 @@ origins = ["http://localhost:3000"]
 app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True,
                    allow_methods=["*"],
                    allow_headers=["*"],)
+
+# print(uuid.uuid4())
+print(datetime.now())
 
 
 class User(BaseModel):
@@ -36,14 +41,14 @@ class User(BaseModel):
 
 
 class Tweet(BaseModel):
-    id: str
+    # id: str
     userId: str
     body: str
     images:  str
-    likeCount: int
-    retweetCount: int
-    replyCount: int
-    createdAt: date
+    # likeCount: int
+    # retweetCount: int
+    # replyCount: int
+    # createdAt: datetime
 
 
 class Retweet(BaseModel):
@@ -57,27 +62,30 @@ class Like(BaseModel):
     id: str
     userId: str
     tweetId: str
-    createdAt: date
+    createdAt: datetime
 
 
-class Reply:
+class Reply(BaseModel):
     id: str
     userId: str
     tweetId: str
     body: str
     images: str
-    createdAt: date
+    createdAt: datetime
 
 
 class Bookmark:
     id: str
     userId: str
     tweetId: str
-    createdAt: str
+    createdAt: datetime
 
-# db_user = User(id="user.id", username="user.username")
 
-# print(db_user)
+class Test(BaseModel):
+    body: str
+# db_tweet = Tweet()
+
+# print(db_tweet)
 
 
 @app.get("/user")
@@ -99,21 +107,22 @@ async def get_user_by_id(user_id: str):
     except:
         return HTTPException(500, "Internal Server Error")
 
- 
+
 @app.get("/tweetuser/{user_id}")
 async def get_tweet_of_user(user_id: str, q: Union[str, None] = None):
     try:
         data = supabase.from_("Tweet").select(
-            "*,User(*)").eq("userId", user_id).execute()
+            "*,User(*),Like(*),Reply(*),Bookmark(*)").eq("userId", user_id).execute()
         return data.data
     except:
         return HTTPException(500, "Internal Server Error")
+
 
 @app.get("/tweet/{tweet_id}")
 async def get_tweet_by_id(tweet_id: str):
     try:
         data = supabase.from_("Tweet").select(
-            "*,User(*),Like(*),Reply(*)").eq("id", tweet_id).single().execute()
+            "*,User(*),Like(*),Reply(*),Bookmark(*)").eq("id", tweet_id).single().execute()
         return data.data
     except:
         return HTTPException(500, "Internal Server Error")
@@ -123,7 +132,7 @@ async def get_tweet_by_id(tweet_id: str):
 async def get_all_tweet():
     try:
         data = supabase.from_("Tweet").select(
-            "*,User(*),Like(*),Reply(*)").execute()
+            "*,User(*),Like(*),Reply(*),Bookmark(*)").execute()
         return data.data
     except:
         return HTTPException(500, "Internal Server Error")
@@ -145,10 +154,49 @@ async def get_random_tweets():
 # loop.close()
 
 
-@app.post('/tweet', response_model=User)
-async def add_user(user: User):
+@app.post('/tweet')
+async def add_tweet(tweet: Tweet):
+    # try:
+    tweet = supabase.from_("Tweet").insert(
+        {"body": tweet.body, "userId": tweet.userId, "images": tweet.images}).execute()
+    #     if tweet:
+    #         return {"message": "Tweet created successfully"}
+    #     else:
+    #         return {"message": "Tweet creation failed"}
+    # except Exception as e:
+    #     print("Error: ", e)
+    #     return e
+
+
+@app.delete("/tweet/{tweet_id}")
+async def deleteTweet(tweet_id: str):
+    supabase.from_("Tweet").delete().eq("id", tweet_id).execute()
+
+
+@app.post("/auth/signout")
+async def sign_out():
+    res = supabase.auth.sign_out()
+    return res
+
+
+@app.get("/usermeta")
+async def get_all_meta_users():
     try:
-        db_user = User(id=user.id, username=user.username)
-        supabase.from_("User").insert({id: user.id}).execute()
+        data = supabase.auth.get_user()
+        return data
     except:
         return HTTPException(500, "Internal Server Error")
+
+
+@app.post('/test')
+async def add_test(test: Test):
+    try:
+        test = supabase.from_("Test").insert(
+            {"body": test.body}).execute()
+        if test:
+            return {"message": "Tweet created successfully"}
+        else:
+            return {"message": "Tweet creation failed"}
+    except Exception as e:
+        print("Error: ", e)
+        return e
